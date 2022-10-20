bf31a1e4d34d1cf7725335908d83802ec29f3cf4
Sean17bcr <106638574+Sean17bcr@users.noreply.github.com>
2022-10-19 18:40:10 -0600

    creacion de proyecto

---
+++ PracticaExamenWEB/src/test/java/com/PracticaExamen/PracticaExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PracticaExamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
