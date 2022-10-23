234c5362e2cbf880cc95f3c2736b6f2ef077dc5f
Kendall-22 <ken.araya.solis@gmail.com>
2022-10-22 19:03:08 -0600

    Practica Completed

--- practica01/src/main/java/com/practica01/controller/IndexController.java
+++ practica01/src/main/java/com/practica01/controller/IndexController.java
@@ -1,13 +1,25 @@
 
 package com.practica01.controller;
 
+import com.practica01.service.EstadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
     
+    @Autowired
+    private EstadoService estadoService;
+    
+    @GetMapping("/")
+    public String inicio(Model model){
+        var texto="Practica Semana 6";
+        model.addAttribute("mensaje", texto);
+        
+        var estados = estadoService.getEstados();
+        model.addAttribute("estados", estados);
+        return"index";
+    }
 }
